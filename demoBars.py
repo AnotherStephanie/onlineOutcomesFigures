ccb4601b979538b6984ad7c88d9d6ecdb62f517b
Stephanie P <37088647+AnotherStephanie@users.noreply.github.com>
2019-04-30 11:45:17 -0700

    Fixed ESL bars
    Fixed error in ESL bar display; laid foundation for a more flexible function, in case I want to expand the analysis later.

--- demoBars.py
+++ demoBars.py
@@ -26,9 +26,9 @@ def makePercent(df = dfGender):
 #set up data frames using demographic numbers
 
 Online = {"Women":238, "Men":178, "Seniors":166, "Non-Seniors":251, 
-          "English Only":192, "English and Another":108, "Another language":117}
+          "English Only":192, "English and Another":108, "Another Language":117}
 inPerson = {"Women":316, "Men":320, "Seniors":91, "Non-Seniors":545, 
-          "English Only":350, "English and Another":143, "Another language":143}
+          "English Only":350, "English and Another":143, "Another Language":143}
 
 dfGender = pd.DataFrame([inPerson, Online], columns=["Men", "Women"], 
                         index=["In Person", "Online"])
@@ -38,7 +38,7 @@ dfStanding = pd.DataFrame([inPerson, Online], columns=["Non-Seniors", "Seniors"]
                         index=["In Person", "Online"])
 
 dfESL =  pd.DataFrame([inPerson, Online], columns=["English Only", 
-                      "English and Another", "Another language"], 
+                      "English and Another", "Another Language"], 
                         index=["In Person", "Online"])
 
 dfGender = makePercent(dfGender)
@@ -55,18 +55,24 @@ dfESL = makePercent(dfESL)
 barWidth = 0.85
 #names
 plt.bar([0, 1], dfGender["Women"], color='#b5ffb9', edgecolor='white', width=barWidth)
-plt.bar([0,1], dfGender["Men"], bottom=dfGender["Women"], color='#f9bc86', edgecolor='white', width=barWidth)     
+plt.bar([0,1], dfGender["Men"], bottom=dfGender["Women"], color='#f9bc86', 
+        edgecolor='white', width=barWidth)     
         
         
 plt.show()
 
-plt.bar([0, 1], dfStanding["Non-Seniors"], color='#b5ffb9', edgecolor='white', width=barWidth)
-plt.bar([0, 1], dfStanding["Seniors"], bottom=dfStanding["Non-Seniors"], color='#f9bc86', edgecolor='white', width=barWidth)     
+plt.bar([0, 1], dfStanding["Non-Seniors"], color='#b5ffb9', edgecolor='white', 
+        width=barWidth)
+plt.bar([0, 1], dfStanding["Seniors"], bottom=dfStanding["Non-Seniors"], 
+        color='#f9bc86', edgecolor='white', width=barWidth)     
            
 plt.show()
 
 #Trouble: need process for third bar.
 plt.bar([0, 1], dfESL["English Only"], color='#b5ffb9', edgecolor='white', width=barWidth)
-plt.bar([0, 1], dfESL["English and Another"], bottom=dfESL["English Only"], color='#f9bc86', edgecolor='white', width=barWidth)     
-plt.bar([0, 1], dfESL["Another language"], bottom=dfESL["English and Another"], color='green', edgecolor='white', width=barWidth)   
+plt.bar([0, 1], dfESL["English and Another"], bottom=dfESL["English Only"], 
+        color='#f9bc86', edgecolor='white', width=barWidth)     
+barBottom = np.add(dfESL["English Only"], dfESL["English and Another"]).tolist()
+plt.bar([0, 1], dfESL["Another Language"], bottom=barBottom,
+        color='green', edgecolor='white', width=barWidth)   
 plt.show()
